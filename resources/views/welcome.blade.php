b5f8ab47c859e67b593f6d598f1aac2cd6e43d34
fray david herrera duarte <148037427+Frayherrera@users.noreply.github.com>
2025-09-25 19:40:29 -0500

    intento6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,8 @@
+@extends('layouts.home')
+@section('content')
 <div>
     @foreach ($noticiasDestacadas as $noticia)
         <x-featured-article :noticia="$noticia" />
     @endforeach
 </div>
+@endsection
