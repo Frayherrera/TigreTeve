b446e77e7339f195deeafa335a21d73539c11bf5
fray david herrera duarte <148037427+Frayherrera@users.noreply.github.com>
2025-11-28 17:34:10 -0500

    resposive

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -6,6 +6,7 @@
     <title>@yield('title', 'Tigre teve')</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/home.css">
+    <script src="{{ asset('js/responsive.js') }}"></script>
     @livewireStyles
 
 </head>
