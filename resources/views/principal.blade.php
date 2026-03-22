5b450b23cb26f142a516270eac5247053c739277
fray david herrera duarte <148037427+Frayherrera@users.noreply.github.com>
2026-03-21 22:51:27 -0500

    s

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -24,8 +24,6 @@
                             <x-blog-card :noticia="$noticia" />
                         @endforeach
                     </div>
-
-
                 </div>
 
                 <!-- Sidebar -->
