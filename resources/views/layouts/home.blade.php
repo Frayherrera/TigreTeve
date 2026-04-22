2772a63ecee68e8013e08e77f4b5b8cc533bca86
Fray Herrera <ufonefray@gmail.com>
2026-04-22 18:38:03 -0500

    pwa

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -3,18 +3,14 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="apple-mobile-web-app-status-bar" content="#01d679">
-    <meta name="apple-mobile-web-app-capable" content="yes">
     <title>@yield('title', 'Tigre teve')</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/home.css">
     <script src="{{ asset('js/responsive.js') }}"></script>
     @livewireStyles
     <link rel="manifest" href="/manifest.json">
+    
     <meta name="theme-color" content="#000000">
-
-    <link href="/icons/icon-192.png" sizes="192x192" rel="apple-touch-startup-image">
-    <link href="/icons/icon-512.png" sizes="512x512" rel="apple-touch-startup-image">
 
 </head>
 <script>
@@ -36,7 +32,6 @@ if ('serviceWorker' in navigator) {
     @include('partials.footer')
     @livewireScripts
 
-     @vite(['resources/js/app.js'])
 </body>
 
 </html>
